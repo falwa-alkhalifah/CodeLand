d4876b38f1abca2b02665e081cf76cd84ee898ae
Deema <demohato@gmail.com>
2025-09-12 00:47:51 +0300

    ي

--- js-deema/data.nomodule.js
+++ js-deema/data.nomodule.js
@@ -12,6 +12,6 @@
   window.recommendedQuestions = [
     { id:9001, topic:"HTML 101", learner:{name:"Sara Abdullah.", photo:"images/WebSA.jpeg"},
       figure:null, text:"Which HTML tag is used to define a paragraph?",
-      choices:["section","div","p","article"], correctIndex:1, status:"Pending" }
+      choices:["section","div","p","article"], correctIndex:2, status:"Pending" }
   ];
 })();
